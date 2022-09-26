0b8a910a394d99b2c51c4810b80e105d496616c5
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-09-26 13:42:19 -0500

    Add GMaps Vue library

---
+++ src/pages/map/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { GoogleMap, Marker } from 'vue3-google-map'
+const center = { lat: 40.689247, lng: -74.044502 }
+const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+</script>
+
+<template>
+  <GoogleMap :api-key="API_KEY" style="width: 100%; height: 500px" :center="center" :zoom="15">
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+</template>
+
