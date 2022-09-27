12895bbb576a2da2b447a08ac6ba4fb6a1a9d43c
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-09-26 20:39:52 -0500

    Add GMaps type definitions

--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -1,12 +1,34 @@
 <script setup lang="ts">
 import { GoogleMap, Marker } from 'vue3-google-map'
-const center = { lat: 40.689247, lng: -74.044502 }
+
+const center = ref({ lat: 40.689247, lng: -74.044502 })
 const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+const gmaps = (ref(null) as unknown) as google.maps.Map
+
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        const geoPosition = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        }
+
+        center.value = geoPosition
+        // gmaps.setCenter(center.value)
+      },
+      error => console.error(error),
+    )
+  }
+}
 </script>
 
 <template>
-  <GoogleMap :api-key="API_KEY" style="width: 100%; height: 500px" :center="center" :zoom="15">
+  <GoogleMap ref="gmaps" :api-key="API_KEY" style="width: 100%; height: 500px" :center="center" :zoom="15">
     <Marker :options="{ position: center }" />
   </GoogleMap>
+  <div class="btn" @click="getLocation">
+    Get current location
+  </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -151,6 +151,7 @@ export default defineConfig({
     script: 'async',
     formatting: 'minify',
     onFinished() { generateSitemap() },
+    format: 'cjs',
   },
 
   ssr: {
