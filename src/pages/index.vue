e5a6b06dfb23775c7e68003ee3fbb2c9f1945aae
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-10-10 22:43:25 -0500

    Mueve view del mapa a la ruta principal

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,62 +1,156 @@
 <script setup lang="ts">
-const user = useUserStore()
-const name = $ref(user.savedName)
+import { GoogleMap, Marker } from 'vue3-google-map'
+import type { ATM } from '~/models/atm/atm'
+import { atmToLatLngLiteral } from '~/models/atm/atm'
+import atmService from '~/services/atms/api/api-atm.service'
+import { toLatLngLiteral } from '~/utils/geolocation'
+import styles from '~/styles/map-style'
 
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
+const center = ref<google.maps.LatLngLiteral>({
+  lat: 19.43302471023951,
+  lng: -99.13224809885025,
+})
+const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+
+const gmaps = ref<InstanceType<typeof GoogleMap>>()
+const currentPosition = ref<GeolocationPosition>()
+// let distanceService: google.maps.DistanceMatrixService
+
+let directionsServices: google.maps.DirectionsService
+let directionsRenderer: google.maps.DirectionsRenderer
+
+// const distanceAPI = ref(0)
+let atmLocations = ref<ATM[]>([])
+
+// Modal dialog
+let modalOpen = ref(false)
+let selectedATM = ref<ATM>()
+
+const positionCallback = (position: GeolocationPosition) => {
+  currentPosition.value = position
+
+  center.value = toLatLngLiteral(currentPosition.value)
+
+  console.log('location has changed')
 }
 
-const { t } = useI18n()
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      positionCallback,
+      (error) => alert(error),
+      {
+        enableHighAccuracy: true,
+      }
+    )
+  }
+}
+
+const getNearATMs = async (location: google.maps.LatLngLiteral) => {
+  const bounds = new google.maps.LatLngBounds()
+
+  try {
+    atmLocations.value = await atmService.getATMs(location)
+
+    atmLocations.value.forEach((atm) => bounds.extend(atmToLatLngLiteral(atm)))
+  } catch (error) {
+    console.error(error)
+  }
+
+  gmaps.value?.map?.fitBounds(bounds)
+  gmaps.value?.map?.panToBounds(bounds)
+}
+
+const getRoute = async () => {
+  await directionsServices.route(
+    {
+      origin: center.value,
+      destination: atmToLatLngLiteral(atmLocations.value[0]),
+      travelMode: google.maps.TravelMode.DRIVING,
+      unitSystem: google.maps.UnitSystem.METRIC,
+    },
+    function (result, status) {
+      if (status == 'OK') {
+        directionsRenderer.setDirections(result)
+      }
+    }
+  )
+}
+
+const showATMInfo = (atm: ATM) => {
+  selectedATM.value = atm
+
+  modalOpen.value = true
+}
+
+onMounted(() => {
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+      positionCallback,
+      (error) => alert(error),
+      {
+        enableHighAccuracy: true,
+      }
+    )
+  }
+})
+
+const closeATMInfo = () => (modalOpen.value = false)
+
+watch(
+  () => gmaps.value?.ready,
+  (ready) => {
+    if (ready) {
+      directionsServices = new google.maps.DirectionsService()
+      directionsRenderer = new google.maps.DirectionsRenderer()
+      directionsRenderer.setMap(gmaps.value?.map || null)
+    }
+  }
+)
 </script>
 
 <template>
-  <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
-    </div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-    </p>
+  <GoogleMap
+    ref="gmaps"
+    :api-key="API_KEY"
+    class="absolute w-full h-full"
+    :center="center"
+    :zoom="15"
+    :styles="styles"
+    language="es-MX"
+  >
+    <!-- @load="onMapsLoaded" -->
+    <Marker
+      :options="{
+        position: center,
+      }"
+    />
+    <Marker
+      v-for="(location, index) in atmLocations"
+      :key="index"
+      :options="{ position: atmToLatLngLiteral(location) }"
+      @click="showATMInfo(location)"
+    />
 
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      :aria-label="t('intro.whats-your-name')"
-      type="text"
-      autocomplete="false"
-      p="x4 y2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
+    <InfoDialog
+      title="Cajero Automático"
+      :show="modalOpen"
+      @close="closeATMInfo"
     >
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        btn m-3 text-sm
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
+      <ATMInfo :atm="selectedATM!" />
+    </InfoDialog>
+  </GoogleMap>
+  <div class="fixed bottom-0 w-full">
+    <div class="flex-inline items-center m-auto justify-start">
+      <div class="btn m-2" @click="getLocation">Actualizar ubicación</div>
+      <div class="btn m-2" @click="getNearATMs(center)">Cajeros cercanos</div>
+      <div class="btn m-2" @click="getRoute()">Mostrar ruta</div>
+      <!-- <div class="btn" @click="calculateDistance">Calculate distance</div> -->
     </div>
   </div>
 </template>
 
 <route lang="yaml">
 meta:
-  layout: home
+  layout: map
 </route>
