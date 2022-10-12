afa97d5c3cb6e8fa450553ad000c5c902774935b
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-10-12 09:04:59 -0500

    Refactoriza RecordService usando promesas

--- src/services/record.service.ts
+++ src/services/record.service.ts
@@ -1,30 +1,57 @@
-import RecordRTC from 'recordrtc'
+import RecordRTC, { RecordRTCPromisesHandler } from 'recordrtc'
 
 export default class RecordService {
-  recorder: RecordRTC
+  recorder: RecordRTCPromisesHandler
 
-  constructor(...args: ConstructorParameters<typeof RecordRTC>) {
-    this.recorder = new RecordRTC(...args)
+  constructor(...args: ConstructorParameters<typeof RecordRTCPromisesHandler>) {
+    this.recorder = new RecordRTCPromisesHandler(...args)
   }
 
-  startRecording() {
-    this.recorder.startRecording()
+  async startRecording() {
+    return this.recorder.startRecording()
   }
 
   async stopRecording() {
-    return new Promise<Buffer>((resolve, reject) => {
-      try {
-        this.recorder.stopRecording(() => {
-          const buffer = Buffer.from(this.recorder.getDataURL(), 'base64')
-          // Reset recorder data
-          this.recorder.reset()
+    return this.recorder.stopRecording()
+  }
 
-          resolve(buffer)
-        })
-      } catch (error) {
-        reject(error)
-      }
-    })
+  async reset() {
+    return this.recorder.reset()
+  }
+
+  async getBuffer() {
+    return Buffer.from(await this.getDataURL(), 'base64')
+  }
+
+  async getBlob() {
+    return this.recorder.getBlob()
+  }
+
+  async getDataURL() {
+    return this.recorder.getDataURL()
+  }
+
+  private async stopAndGet<T>(func: () => T, reset = false, ...funcArgs: never[]) {
+    await this.stopRecording()
+    const data = func.bind(this, ...funcArgs)()
+
+    if (reset) {
+      this.reset()
+    }
+
+    return data
+  }
+
+  async stopAndGetBuffer(reset = false) {
+    return this.stopAndGet(this.getBuffer, reset)
+  }
+
+  async stopAndGetBlob(reset = false) {
+    return this.stopAndGet(this.getBlob, reset)
+  }
+
+  async stopAndGetDataURL(reset = false) {
+    return this.stopAndGet(this.getDataURL, reset)
   }
 }
 
