8e146e05033c36135acec1ca557d9da37110ea58
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-10-13 08:06:24 -0500

    Elimina DialogFlowCX service

--- src/client.d.ts
+++ src/client.d.ts
@@ -2,11 +2,13 @@
 
 interface ImportMetaEnv {
   readonly VITE_GOOGLE_MAPS_API_KEY: string
+
   // RecordRTC recorder
   readonly VITE_RECORDER_MIME_TYPE: string
   readonly VITE_RECORDER_SAMPLE_RATE: number
   readonly VITE_RECORDER_DESIRED_SAMPLE_RATE: number
   readonly VITE_RECORDER_AUDIO_CHANNELS: number
+
   // DialogFlow audio encoding
   readonly VITE_DIALOGFLOW_AUDIO_ENCODING: string
   readonly VITE_DIALOGFLOW_AUDIO_SAMPLE_RATE: number
