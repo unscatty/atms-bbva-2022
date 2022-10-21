2f6c49c37e432a7b23c99f853dbfb280df1cf406
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-10-21 08:10:12 -0500

    Agrega servicio de Firebase

--- src/client.d.ts
+++ src/client.d.ts
@@ -17,6 +17,14 @@ interface ImportMetaEnv {
   // Server
   readonly VITE_SERVER_ENDPOINT: string
   readonly VITE_SERVER_SOCKET_ENDPOINT: string
+
+  // Firebase config
+  readonly VITE_FIREBASE_API_KEY: string
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string
+  readonly VITE_FIREBASE_PROJECT_ID: string
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
+  readonly VITE_FIREBASE_APP_ID: string
   // more env variables...
 }
 
